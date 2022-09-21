12348b0f18865e7ad916ea4f27d0f203e86f2c3c
wang8456 <909248795@qq.com>
2022-09-21 16:51:25 +0800

    Site updated: 2022-09-21 16:51:25

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,6 @@ function percent() {
         btn = document.querySelector("#percent"); // 获取图标
 
         result<=99||(result=99),btn.innerHTML=result
-}
 
 document.getElementById("page-name").innerText=document.title.split(" | 安知鱼")[0];
+}
