b71325784027a880a08a377be23dd8f58156b74c
unknown <909248795@qq.com>
2024-02-25 22:33:51 +0800

    Site updated: 2024-02-25 22:33:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/c0a9.html","posts/df9c.html","posts/635a.html","posts/bf8s.html","posts/9b31.html","posts/9b31.html","posts/f0e2.html","posts/bf8f.html","posts/8e5d.html","posts/2215.html","posts/9743.html","posts/7af8.html","posts/129b.html"];function toRandomPost(){
+var posts=["posts/c0a9.html","posts/df9c.html","posts/635a.html","posts/bf8s.html","posts/9b31.html","posts/f0e2.html","posts/9b31.html","posts/bf8f.html","posts/8e5d.html","posts/2215.html","posts/9743.html","posts/129b.html","posts/7af8.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
