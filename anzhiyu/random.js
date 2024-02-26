6bc52179a3d5c26f0fbba3a6f99ef3ebcdd07430
unknown <909248795@qq.com>
2024-02-26 18:20:37 +0800

    Site updated: 2024-02-26 18:20:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/c0a9.html","posts/df9c.html","posts/6f9.html","posts/cabd.html","posts/bf8s.html","posts/635a.html","posts/9b31.html","posts/f0e2.html","posts/bf8f.html","posts/8e5d.html","posts/2215.html","posts/9743.html","posts/129b.html","posts/7af8.html"];function toRandomPost(){
+var posts=["posts/c0a9.html","posts/df9c.html","posts/cabd.html","posts/6f9.html","posts/635a.html","posts/bf8s.html","posts/463d.html","posts/9b31.html","posts/f0e2.html","posts/8e5d.html","posts/bf8f.html","posts/2215.html","posts/9743.html","posts/463d.html","posts/129b.html","posts/7af8.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
